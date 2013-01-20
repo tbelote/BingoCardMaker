26709ec6a5829edc638dc0774a207e6a1c55992d
Thomas Belote <tom@kincast.com>
2013-01-20 00:04:25 -0800

    Revert "Revert "intial import""
    This reverts commit a3097ae50ae82b3ea5cd35b6555045c2918b64b8.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BingoCardMaker
+//
+//  Created by Thomas Belote on 12/31/12.
+//  Copyright (c) 2012 Thomas Belote. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
